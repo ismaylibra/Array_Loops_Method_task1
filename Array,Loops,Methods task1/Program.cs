33b3e286b6610c22a5fc34e5ca2052dde410e406
[ismaylibra] <[ismayiliib@code.edu.az]>
2022-04-14 17:22:00 +0400

    homework 1 done and added

---
+++ Array,Loops,Methods task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Array_Loops_Methods_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Soz daxil edin");
+            string word1 = Console.ReadLine();
+            Console.WriteLine("Soz daxil edin");
+            string word2 = Console.ReadLine();
+            Console.WriteLine("Soz daxil edin");
+            string word3 = Console.ReadLine();
+            string[] words = { word1, word2, word3 };
+            int i = 0;
+            foreach (string word in words)
+            {
+                if (words[i].Length > 5)
+                {
+                    Console.WriteLine("Uzunlugu 5-den boyuk olan sozler");
+                    Console.WriteLine(words[i]);
+                }
+                else { };
+                i++;
+            }
+        }
+
+        }
+
+    }
